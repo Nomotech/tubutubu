9964b879b1c8212fe10997315009a560bc15a556
nomotech <nmtts.1126@gmail.com>
2017-05-23 19:55:57 +0900

    熱

--- function.js
+++ function.js
@@ -41,6 +41,280 @@ document.onmouseup = function (e){
 	mouseR = false;
 };
 
+//---------------------------------< keyboard >--------------------------------------------
+//keycode
+var key = new Array(300);
+var keyName = [
+	"",
+	"Mouse L", 
+	"Mouse R", 
+	"Cancel", 
+	"Mouse M", 
+	"", 
+	"", 
+	"", 
+	"BackSpace", 
+	"Tab",
+	"", 
+	"", 
+	"Clear", 
+	"Enter", 
+	"", 
+	"", 
+	"Shift", 
+	"Ctrl", 
+	"Alt",
+	"Pause", 
+	"CapsLock",
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"Esc", 
+	"", 
+	"",
+	"", 
+	"", 
+	"Space", 
+	"PageUp", 
+	"PageDown", 
+	"End", 
+	"Home", 
+	"Left", 
+	"Up", 
+	"Right",
+	"Down",
+	"", 
+	"",
+	"", 
+	"PrtSc", 
+	"Insert", 
+	"Delete", 
+	"Help", 
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"",
+	":",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"Ten 0", 
+	"Ten 1", 
+	"Ten 2",
+	"Ten 3", 
+	"Ten 4", 
+	"Ten 5", 
+	"Ten 6", 
+	"Ten 7", 
+	"Ten 8",
+	"Ten 9",
+	"*",
+	"+", 
+	"Enter", 
+	"-", 
+	".", 
+	"/", 
+	"F1",
+	"F2", 
+	"F3", 
+	"F4", 
+	"F5", 
+	"F6", 
+	"F7", 
+	"F8", 
+	"F9", 
+	"F10", 
+	"F11", 
+	"F12", 
+	"F13", 
+	"F14", 
+	"F15", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"NumLock", 
+	"ScrLk", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"",
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	": *", 
+	"; +", 
+	", <", 
+	"- =", 
+	". >", 
+	"/ ?", 
+	"@ `", 
+	"", 
+	"", 
+	"", 
+	"",
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"[ {", 
+	"\|",
+	"] }", 
+	"^ ~", 
+	"",
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	"", 
+	""
+];
+var keyCharacter = "";
+document.onkeydown = function (e){
+  if(!e)e = window.event;
+  //console.log(e);
+  key[e.keyCode] = 1;
+  keyCharacter = keyName[e.keyCode];
+  console.log(keyCharacter + ": " + e.keyCode);
+}
+document.onkeyup = function (e){
+  if(!e)e = window.event;
+  key[e.keyCode] = 0;
+  keyCharacter = "";
+}
 
 //---------------------------------< length >----------------------------------------------
 function lengthTwoPoint(x1,y1,x2,y2){
@@ -81,18 +355,57 @@ function lengthWall(cir,wa){
 function lengthWall2(cir,wa){
 	//console.log(d);
 	if(wa.length!=0){
-		var length_ = 1/wa.length;
+		var length_inv = 1/wa.length;
 		var v ={};
 		v.x = cir.x - wa.x1;
 		v.y = cir.y - wa.y1;
 		var u ={};
 		u.x = wa.x2 - wa.x1;
 		u.y = wa.y2 - wa.y1;
-		var length = (u.x * v.y - u.y * v.x)/wa.length;
+		var length = (u.x * v.y - u.y * v.x) * length_inv;
 		if(length<0) console.log("po");
 		return length;
 	}
 	return 0;
+}
+
+function lengthPointLine(p,l){
+	if(l.length == 0) return 0;
+	var v = {};
+	var u = {}; 
+	var cos;
+	
+	u.x = l.x2 - l.x1;
+	u.y = l.y2 - l.y1;
+	
+	v.x = p.x - l.x2;
+	v.y = p.y - l.y2;
+	v.length = Math.sqrt(v.x * v.x + v.y * v.y);
+	cos = (v.x * u.x + v.y * u.y) / (v.length * l.length);
+	//console.log("cos2: " + cos);
+	if(cos > 0){
+		v.inv = 1 / v.length;
+		return v;
+	}
+
+	v.x = p.x - l.x1;
+	v.y = p.y - l.y1;
+	v.length = Math.sqrt(v.x * v.x + v.y * v.y);
+	cos = (v.x * u.x + v.y * u.y) / (v.length * l.length);
+	//console.log("cos1: " + cos);
+	if(cos < 0){
+		v.inv = 1 / v.length;
+		return v;
+	}
+
+	var d = {};
+	cos = (v.x * u.x + v.y * u.y) / (l.length * l.length);
+	d.x = v.x - cos * u.x;
+	d.y = v.y - cos * u.y;
+	d.length = Math.sqrt(d.x * d.x + d.y * d.y); 
+	d.inv = 1 / d.length;
+	//v.length = (u.x * v.y - u.y * v.x) / l.length;
+	return d;
 }
 
 //----------------------------------< draw >-----------------------------------------------
@@ -104,7 +417,8 @@ function drawLine(x1,y1,x2,y2,color){
     ctx.stroke();       // 4.Canvas上に描画する
 }
 
-function drawBox(x,y,w,h) {
+function drawBox(x,y,w,h,color) {
+	ctx.strokeStyle = color; 
     ctx.beginPath();
     ctx.strokeRect(x-w/2, y-h/2, w, h);
 }
